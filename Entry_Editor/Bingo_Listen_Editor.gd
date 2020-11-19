4b4e14a6aceb2c4a84675a05a73fe5257440f5de
Ronney <reneator@googlemail.com>
2020-11-19 18:42:53 +0100

    List_Editor: Added Edit functionality
    also simplified the way the entries are being stored (now the entry-entitties themselves hold the text, instead of the text also being saved to an array)

--- Entry_Editor/Bingo_Listen_Editor.gd
+++ Entry_Editor/Bingo_Listen_Editor.gd
@@ -3,27 +3,26 @@ extends Control
 
 export (PackedScene) var List_Element
 
-var entries = []
+var edit_entry : List_Entry #if this value is set the input line instead edits the line_entry
 
 func _ready():
 	load_state()
 
 func initialize(_entries):
-	entries = _entries
-	if not entries:
-		entries = []
-	for entry in entries:
+	for entry in _entries:
 		create_entry(entry)
 
 func create_entry(text):
 	var entry = List_Element.instance()
 	entry.connect("removed", self, "on_entry_removed")
+	entry.connect("edit", self, "on_entry_edit")
 	entry.text = text
 	$VBoxContainer/Entries.add_child(entry)
 
-func on_entry_removed(text):
-	entries.remove(text)
-
+func on_entry_edit(entry : List_Entry):
+	edit_entry = entry
+	$VBoxContainer/HBoxContainer/LineEdit.text = entry.text
+	
 func _on_Button_pressed():
 	clear_error()
 	var text = $VBoxContainer/HBoxContainer/LineEdit.text
@@ -33,16 +32,29 @@ func process_text(text):
 	var valid = valid_text(text)
 	if not valid:
 		return
-	entries.append(text)
 	create_entry(text)
 	
 func valid_text(text):
 	if text == "" or not text:
 		return false
-	if entries.has(text):
+	if has_text(text):
 		return false
 	
 	return true
+
+func get_entries():
+	return $VBoxContainer/Entries.get_children()
+	
+func get_entries_as_strings():
+	var entries = []
+	for child in get_entries():
+		entries.append(child.text)
+	return entries
+
+func has_text(text):
+	for child in get_entries():
+		if child.text == text:
+			return true
 
 func clear_error():
 	$VBoxContainer/Error_Label.clear()
@@ -51,14 +63,20 @@ func print_error(text):
 	$VBoxContainer/Error_Label.print_error(text)
 
 func _on_LineEdit_text_entered(new_text):
-	process_text(new_text)
+	$VBoxContainer/HBoxContainer/LineEdit.text = ""
+	if edit_entry:
+		edit_entry = null
+	else:
+		process_text(new_text)
 
+func _on_LineEdit_text_changed(new_text):
+	if edit_entry:
+		edit_entry.text = new_text
 
 func _on_OK_Button_pressed():
 	save()
 	Global.save()
 	get_tree().change_scene_to(Scenes.create_lobby_screen)
-
 
 func _on_Import_Button_pressed():
 	pass # Replace with function body.
@@ -68,7 +86,7 @@ func _on_Export_Button_pressed():
 	pass # Replace with function body.
 
 func save():
-	Global.entries = entries
+	Global.entries = get_entries_as_strings()
 
 func load_state():
 	var entries_list = Global.entries
--- Entry_Editor/List_Entry.gd
+++ Entry_Editor/List_Entry.gd
@@ -1,8 +1,14 @@
 extends HBoxContainer
+class_name List_Entry
 
-var text
+var text setget set_text
 
 signal removed(text)
+signal edit(list_entry) #self
+
+func set_text(_text):
+	text = _text
+	$Label.text = text
 
 func _ready():
 	$Label.text = text
@@ -10,3 +16,6 @@ func _ready():
 func _on_Button_pressed():
 	emit_signal("removed", text)	
 	queue_free()
+
+func _on_Edit_Button_pressed():
+	emit_signal("edit", self)
